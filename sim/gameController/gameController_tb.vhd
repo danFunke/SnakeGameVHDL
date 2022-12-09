5a48fc067aa60375eaab48bc716ea5cfcf58cfeb
DFunke <dcfunke8@gmail.com>
2022-12-09 01:27:52 -0500

    (wip) gameController

--- sim/gameController/gameController_tb.vhd
+++ sim/gameController/gameController_tb.vhd
@@ -8,16 +8,53 @@ architecture behavior of gameController_tb is
     -- Declare internal components
     component gameController
         port (
+            -- Inputs
             clk     : in STD_LOGIC;
             clr     : in STD_LOGIC;
-            btn3    : in STD_LOGIC
+            btn3    : in STD_LOGIC;
+            gmClk   : in STD_LOGIC;
+
+            -- Outputs
+            headAddrSel     : out STD_LOGIC;
+            headAddrLd      : out STD_LOGIC;
+            headBlkSel      : out STD_LOGIC_VECTOR (2 downto 0);
+            headBlkLd       : out STD_LOGIC;
+            dirSel          : out STD_LOGIC;
+            dirLd           : out STD_LOGIC;
+            lenSel          : out STD_LOGIC;
+            lenLd           : out STD_LOGIC;
+            indexSel        : out STD_LOGIC;
+            indexLd         : out STD_LOGIC;
+            sRAM_we         : out STD_LOGIC;
+            sRAM_oe         : out STD_LOGIC;
+            sRAM_addrSel    : out STD_LOGIC;
+            bodyAddrLd      : out STD_LOGIC;
+            sRAM_DataSel    : out STD_LOGIC
         );
     end component;
 
-    -- Delcare test signals
+    -- Test input signals
     signal clk_tb   : STD_LOGIC;
     signal clr_tb   : STD_LOGIC;
     signal btn3_tb  : STD_LOGIC;
+    signal gmClk_tb : STD_LOGIC;
+
+    -- Test output signals
+    signal headAddrSel_tb   : STD_LOGIC;
+    signal headAddrLd_tb    : STD_LOGIC;
+    signal headBlkSel_tb    : STD_LOGIC_VECTOR (2 downto 0);
+    signal headBlkLd_tb     : STD_LOGIC;
+    signal dirSel_tb        : STD_LOGIC;
+    signal dirLd_tb         : STD_LOGIC;
+    signal lenSel_tb        : STD_LOGIC;
+    signal lenLd_tb         : STD_LOGIC;
+    signal indexSel_tb      : STD_LOGIC;
+    signal indexLd_tb       : STD_LOGIC;
+    signal sRAM_we_tb       : STD_LOGIC;
+    signal sRAM_oe_tb       : STD_LOGIC;
+    signal sRAM_addrSel_tb  : STD_LOGIC;
+    signal bodyAddrLd_tb    : STD_LOGIC;
+    signal sRAM_dataSel_tb  : STD_LOGIC;
 
     -- Define constants
     constant period : time := 10 ns;
@@ -26,9 +63,28 @@ architecture behavior of gameController_tb is
         -- Port map internal components
         uut : gameController
             port map (
+                -- Inputs
                 clk     => clk_tb,
                 clr     => clr_tb,
-                btn3    => btn3_tb
+                btn3    => btn3_tb,
+                gmClk   => gmClk_tb,
+
+                -- Outputs
+                headAddrSel     => headAddrSel_tb,
+                headAddrLd      => headAddrLd_tb,
+                headBlkSel      => headBlkSel_tb,
+                headBlkLd       => headBlkLd_tb,
+                dirSel          => dirSel_tb,
+                dirLd           => dirLd_tb,
+                lenSel          => lenSel_tb,
+                lenLd           => lenLd_tb,
+                indexSel        => indexSel_tb,
+                indexLd         => indexLd_tb,
+                sRAM_we         => sRAM_we_tb,
+                sRAM_oe         => sRAM_oe_tb,
+                sRAM_addrSel    => sRAM_addrSel_tb,
+                bodyAddrLd      => bodyAddrLd_tb,
+                sRAM_dataSel    => sRAM_dataSel_tb
             );
 
         -- Standard clock process
@@ -40,10 +96,20 @@ architecture behavior of gameController_tb is
                 wait for (period / 2);
         end process clock;
 
+        -- Game clock process
+        gmClock : process
+            begin
+                gmClk_tb <= '0';
+                wait for (period * 5);
+                gmClk_tb <= '1';
+                wait for (period * 5);
+        end process gmClock;
+
         -- Test process
         test : process
             begin
                 clr_tb <= '1';
+                btn3_tb <= '0';
                 wait for period;
                 
                 -- Clock cycle 1
--- src/gameController/gameController.vhd
+++ src/gameController/gameController.vhd
@@ -4,9 +4,28 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity gameController is 
     port (
+        -- Inputs
         clk     : in STD_LOGIC;
         clr     : in STD_LOGIC;
-        btn3    : in STD_LOGIC
+        btn3    : in STD_LOGIC;
+        gmClk   : in STD_LOGIC;
+
+        -- Outputs
+        headAddrSel     : out STD_LOGIC;
+        headAddrLd      : out STD_LOGIC;
+        headBlkSel      : out STD_LOGIC_VECTOR (2 downto 0);
+        headBlkLd       : out STD_LOGIC;
+        dirSel          : out STD_LOGIC;
+        dirLd           : out STD_LOGIC;
+        lenSel          : out STD_LOGIC;
+        lenLd           : out STD_LOGIC;
+        indexSel        : out STD_LOGIC;
+        indexLd         : out STD_LOGIC;
+        sRAM_we         : out STD_LOGIC;
+        sRAM_oe         : out STD_LOGIC;
+        sRAM_addrSel    : out STD_LOGIC;
+        bodyAddrLd      : out STD_LOGIC;
+        sRAM_dataSel    : out STD_LOGIC
     );
 end gameController;
 
@@ -26,22 +45,77 @@ architecture behavior of gameController is
                 end if;
         end process synch;
 
-        C1: process (current_state, btn3)
+        C1: process (current_state, btn3, gmClk)
             begin
                 case current_state is
+                    -- Wait for user to press "start" button
                     when state0 =>
                         if (btn3 = '1') then
                             next_state <= state1;
                         else
                             next_state <= state0;
                         end if;
+
+                    -- Load initial game values   
                     when state1 =>
                         next_state <= state2;
+
+                    -- Write initial head value to snake RAM
                     when state2 =>
                         next_state <= state3;
+
+                    -- Write initial tail value to snake RAM
                     when state3 =>
                         next_state <= state0;
                 end case;
         end process C1;
+
+        C2: process (current_state)
+            begin
+                -- Assign default values
+                headAddrSel     <= '0';
+                headAddrLd      <= '0';
+                headBlkSel      <= "000";
+                headBlkLd       <= '0';
+                dirSel          <= '0';
+                dirLd           <= '0';
+                lenSel          <= '0';
+                lenLd           <= '0';
+                indexSel        <= '0';
+                indexLd         <= '0';
+                sRAM_we         <= '0';
+                sRAM_oe         <= '0';
+                sRAM_addrSel    <= '0';
+                bodyAddrLd      <= '0';
+                sRAM_dataSel    <= '0';
+
+                -- State1: initialize game values
+                if (current_state = state1) then
+                    headAddrSel <= '1';
+                    headAddrLd  <= '1';
+                    headBlkLd   <= '1';
+                    dirSel      <= '1';
+                    dirLd       <= '1';
+                    lenSel      <= '1';
+                    lenLd       <= '1';
+                    indexSel    <= '1';
+                    indexLd     <= '1';
+                end if; -- State1
+
+                -- State2: initialize head value in sRAM
+                if (current_state = state2) then
+                    sRAM_we     <= '1';
+                    indexLd     <= '1';
+                    bodyAddrLd  <= '1';
+                end if; -- State2
+
+                -- State3: initialize tail value in sRAM
+                if (current_state = state3) then
+                    sRAM_addrSel    <= '1';
+                    sRAM_we         <= '1';
+                    sRAM_dataSel    <= '1';
+                end if; -- State3
+
+        end process C2;
 
 end behavior;
